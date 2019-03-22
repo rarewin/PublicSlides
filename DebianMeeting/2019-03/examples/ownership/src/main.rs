7dc805618e373bfcce71cd61327e4fd058cc3422
Katsuki Kobayashi <rare@tirasweel.org>
2019-03-23 01:00:39 +0900

    2019-03-22まで

--- DebianMeeting/2019-03/examples/ownership/src/main.rs
+++ DebianMeeting/2019-03/examples/ownership/src/main.rs
@@ -1,5 +1,29 @@
-fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+fn consume(_s: String) {}
+
+fn generate(i: i32) -> Vec<i32> {
+    let mut v = Vec::new();
+    v.push(i);
+    return v;
 }
 
-fn main() {}
+fn main() {
+    let i0 = 1;
+    let _i1 = i0;
+    let _i2 = i0;
+
+    let s0: String = "hoge".to_string();
+    let _s1 = s0;
+    // let _s2 = s0;
+
+    let h: String = "Hello World".to_string();
+    consume(h);
+    // let hh = h;
+
+    let v1 = generate(10);
+    let mut v2 = generate(100);
+
+    v2.push(101);
+
+    println!("v1: {:?}", v1);
+    println!("v2: {:?}", v2);
+}
--- DebianMeeting/2019-03/examples/string/src/main.rs
+++ DebianMeeting/2019-03/examples/string/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    let a = "Hello";
-    let mut b = a.to_string();
+    let mut a = "abcd".to_string(); // String::from("abcd"); も可
+    let b = &a[1..];
+    let /* mut */ c = "zyxwv";
 
     println!("a: {}", a);
     println!("b: {}", b);
+    println!("c: {}", c);
 
-    b.push_str(" World");
-    println!("b: {}", b);
+    a.push_str("efg");
+    println!("a: {}", a);
+
+    // no method named `push_str` found for type `&str` in the current scope
+    // c.push_str("ut");
 }
